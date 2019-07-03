4ecd785b68ab5cb8804ce318cd7a45664ef78545
jia.fu2013@gmail.com <dream2012>
2019-07-04 02:52:22 +0800

    qss

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QApplication,QPushButton,QWidget,QPushButton,QCheckBox,QSlider
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QCheckBox,QSlider,QLabel
+from PyQt5.QtGui import QIcon,QPixmap
 from PyQt5.QtCore import Qt
 
 app = QApplication([])
@@ -16,6 +16,8 @@ window.button = QPushButton('点击我',window)
 
 window.button.move(50,50)
 
+window.button.setStyleSheet("QPushButton{background:green;color:red;font-size:20px;}")
+
 window.checkbox = QCheckBox("我是checkbox",window)
 
 window.checkbox.move(50,100)
@@ -23,6 +25,17 @@ window.checkbox.move(50,100)
 window.qslider = QSlider(Qt.Horizontal,window)
 
 window.qslider.move(50,200)
+
+window.label = QLabel('label',window)
+
+window.label.setGeometry(50,250,50,50)
+
+pixmap = QPixmap('bg3.jpg')
+
+window.label.setPixmap(pixmap)
+
+window.label.setScaledContents(True)
+
 
 window.show()
 
